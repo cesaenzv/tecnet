730802fa3048cba437e661c38a2d98f11aaeb297
dimortizp <dimortizp@ieee.org>
2013-06-16 20:16:10 -0500

    inicio control de usuarios, y crud con los módulos completos

--- protected/config/main.php
+++ protected/config/main.php
@@ -6,83 +6,83 @@
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
 return array(
-	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+'name'=>'My Web Application',
 
-	// preloading 'log' component
-	'preload'=>array('log'),
+// preloading 'log' component
+'preload'=>array('log'),
 
-	// autoloading model and component classes
-	'import'=>array(
-		'application.models.*',
-		'application.components.*',
-		'application.modules.user.models.*',
+// autoloading model and component classes
+'import'=>array(
+'application.models.*',
+'application.components.*',
+'application.modules.user.models.*',
         'application.modules.user.components.*',
         'application.modules.rights.*',
         'application.modules.rights.components.*',
-	),
+),
 
-	'modules'=>array(
-		// uncomment the following to enable the Gii tool
-		
-        'gii' => array(
-            'class' => 'system.gii.GiiModule',
-            'password' => 'clave123456',
-            // If removed, Gii defaults to localhost only. Edit carefully to taste.
-            'ipFilters' => array('127.0.0.1', '::1'),
-        ),
-		'user'=>array(
+'modules'=>array(
+// uncomment the following to enable the Gii tool
+/*
+'gii'=>array(
+'class'=>'system.gii.GiiModule',
+'password'=>'Enter Your Password Here',
+// If removed, Gii defaults to localhost only. Edit carefully to taste.
+'ipFilters'=>array('127.0.0.1','::1'),
+),
+*/
+'user'=>array(
                 'tableUsers' => 'users',
                 'tableProfiles' => 'profiles',
                 'tableProfileFields' => 'profiles_fields',
-                'connectionID'=>'db',
+        ),
         'rights'=>array(
                 'install'=>true,
         ),
-	),
+),
 
-	// application components
-	'components'=>array(
-		// uncomment the following to enable URLs in path-format
-		/*
-		'urlManager'=>array(
-			'urlFormat'=>'path',
-			'rules'=>array(
-				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
-				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
-			),
-            'showScriptName' => false
-		),
-		*/
+// application components
+'components'=>array(
+// uncomment the following to enable URLs in path-format
+/*
+'urlManager'=>array(
+'urlFormat'=>'path',
+'rules'=>array(
+'<controller:\w+>/<id:\d+>'=>'<controller>/view',
+'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+),
+),
+*/
         'db' => array(
             'connectionString' => 'mysql:host=localhost;dbname=tecnet',
             'emulatePrepare' => true,
             'username' => 'tecnetuser',
             'password' => 'ZYt5L9jA5PjLTrsa',
             'charset' => 'utf8',
-			'tablePrefix' => 'tbl_',
+'tablePrefix' => 'tbl_',
         ),
-		'errorHandler'=>array(
-			// use 'site/error' action to display errors
-			'errorAction'=>'site/error',
-		),
-		'log'=>array(
-			'class'=>'CLogRouter',
-			'routes'=>array(
-				array(
-					'class'=>'CFileLogRoute',
-					'levels'=>'error, warning',
-				),
-				// uncomment the following to show log messages on web pages
-				/*
-				array(
-					'class'=>'CWebLogRoute',
-				),
-				*/
-			),
-		),
-		'user'=>array(
+'errorHandler'=>array(
+// use 'site/error' action to display errors
+'errorAction'=>'site/error',
+),
+'log'=>array(
+'class'=>'CLogRouter',
+'routes'=>array(
+array(
+'class'=>'CFileLogRoute',
+'levels'=>'error, warning',
+),
+// uncomment the following to show log messages on web pages
+/*
+array(
+'class'=>'CWebLogRoute',
+),
+*/
+),
+),
+'user'=>array(
                 'class'=>'RWebUser',
                 // enable cookie-based authentication
                 'allowAutoLogin'=>true,
@@ -93,12 +93,12 @@ return array(
                 'connectionID'=>'db',
                 'defaultRoles'=>array('Authenticated', 'Guest'),
         ),
-	),
+),
 
-	// application-level parameters that can be accessed
-	// using Yii::app()->params['paramName']
-	'params'=>array(
-		// this is used in contact page
-		'adminEmail'=>'webmaster@example.com',
-	),
+// application-level parameters that can be accessed
+// using Yii::app()->params['paramName']
+'params'=>array(
+// this is used in contact page
+'adminEmail'=>'webmaster@example.com',
+),
 );
