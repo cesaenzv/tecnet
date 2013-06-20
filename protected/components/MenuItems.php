638abde2e7979e99316be0e468a794cc099df355
Carlos Ernesto Sáenz Vega <cesaenzv@hotmail.com>
2013-06-20 00:13:54 -0500

    Adecuación menu

--- protected/components/MenuItems.php
+++ protected/components/MenuItems.php
@@ -15,24 +15,28 @@ class MenuItems{
                 return $menuitems;
             }
             else{
-            $tasks= Authitemchild::model()->findAll("parent='".$rol."'");           
-            foreach ($tasks as $task){
-                $task_menu = array('label'=>$task->child);
-                $operations = Authitemchild::model()->findAll("parent='".$task->child."'");
-                $labelParent = str_replace( ".*", "",$task->child);
-                $operations_task = array();
-                foreach($operations as $operation){                    
-                    $url = strtolower(str_replace( ".", "/",$operation->child));
-                    $labelSon = str_replace( $labelParent.'.', "",$operation->child);
-                    $operations_task[] = array('label' => $labelSon, 'url' => array($url));
-                }                    
-                if (! empty($operations_task)){
-                    $menu_items[] = array('label'=>$labelParent, 'items'=>$operations_task);
+                if($rol == 'Admin'){
+                    $menu_items[] = array(  'label'=>'Rights', 'url'=>array('/rights'), 
+                                            'visible'=>!Yii::app()->user->isGuest);
                 }
-            }            
+                $tasks= Authitemchild::model()->findAll("parent='".$rol."'");           
+                foreach ($tasks as $task){
+                    $task_menu = array('label'=>$task->child);
+                    $operations = Authitemchild::model()->findAll("parent='".$task->child."'");
+                    $labelParent = str_replace( ".*", "",$task->child);
+                    $operations_task = array();
+                    foreach($operations as $operation){                    
+                        $url = strtolower(str_replace( ".", "/",$operation->child));
+                        $labelSon = explode(".",$operation->child);
+                        $operations_task[] = array('label' => $labelSon[1], 'url' => array($url));
+                    }                    
+                    if (! empty($operations_task)){
+                        $menu_items[] = array('label'=>$labelParent, 'items'=>$operations_task);
+                    }
+                }            
     //        var_dump(Yii::app()->getModule('user')->logoutUrl);
     //        die();
-            return $menu_items;
+                return $menu_items;
             }
         
         }
