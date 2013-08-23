74796e7f26e1329dfddb84ed6a6c59de942c8f8f
Carlos Ernesto Sáenz Vega <cesaenzv@hotmail.com>
2013-08-23 00:13:44 -0500

    Save equipo
    Modificacion dropdownlist

--- protected/models/ManageModel.php
+++ protected/models/ManageModel.php
@@ -2,10 +2,10 @@
 
 class ManageModel {
 
-	public function getColumnList($modelList, $attr){
+	public function getColumnList($modelList, $id, $text){
 		$list = array();
 		foreach ($modelList as $model) {
-			$list[] = $model->$attr;
+			$list[$model->$id] = $model->$text;
 		}
 		return $list;
 	}
